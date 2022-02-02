598f565199451ebcf04de867cf27926c30c0f307
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2022-02-02 00:33:55 -0300

    Roboto font family improvement for Tailwind CSS

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,6 @@ export default class AppDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=auto"
-            rel="stylesheet"
-          />
           <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
           <meta name="author" content="Felipe Pereira de Souza Silva" />
           <meta name="theme-color" content="#121212" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        body: ['Roboto']
+      },
       colors: {
         primary: '#0066ff'
       }
